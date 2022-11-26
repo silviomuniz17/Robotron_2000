e82a8de03883e54b64fc47f4a856c068377581be
silviomuniz17 <silvio.carlos.17@hotmail.com>
2022-11-26 18:52:44 -0300

    Fim

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 // criando uma varaivel que tras o valor de uma classe (.) usar o all vai trazer todas classescom esse nome
 // criando uma variavel que tras o valor de uma propiedade do tipo data
 
-const controle = document.querySelectorAll("[data-controle]");
-const estatisticas = document.querySelectorAll("[data-estatistica]");
+const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
 const pecas = {
     "bracos": {
         "forca": 29,
@@ -18,19 +18,19 @@ const pecas = {
         "energia": 0,
         "velocidade": -20
     },
-    "nucleos": {
+    "nucleos":{
         "forca": 0,
         "poder": 7,
         "energia": 48,
         "velocidade": -24
     },
-    "pernas": {
+    "pernas":{
         "forca": 27,
         "poder": 21,
         "energia": -32,
         "velocidade": 42
     },
-    "foguetes": {
+    "foguetes":{
         "forca": 0,
         "poder": 28,
         "energia": 0,
@@ -38,34 +38,34 @@ const pecas = {
     }
 }
 
-// criando um console que recebe os dados do elemento click
-controle.forEach((elemento) => {
-    elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
-        atualizaEstatisticas(evento.target.dataset.peca);
-    })
-})
-
 function trocaImagem(cor){
     document.querySelector(".robo").src="Robotrons/Robotron_2000_" + cor + "/robotron.png";
 }
 
-function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector("[data-contador]");
+// criando um console que recebe os dados do elemento click
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.peca)
+    })
+})
 
-    if (operacao === "-") {
-        peca.value = parseInt(peca.value) - 1;
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]")
+
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
     } else {
-        peca.value = parseInt(peca.value) + 1;
+        peca.value = parseInt(peca.value) + 1
     }
 }
 
-function atualizaEstatisticas(peca) {
+function atualizaEstatistica(peca) {
     // trazendo o valor que tem na variavel criada referente a onde foi clicado
     console.log(pecas[peca]);
 
     //forEach é um arrei para percorrer todos os elemetos assim consigo atualizar o valores de tudo
-    estatisticas.forEach( (elemento ) => {
+    estatistica.forEach( (elemento ) => {
         //com esse log consigo ver o nome da estatistica que precisa atualizar
         console.log(elemento.dataset.estatistica);
         //esse console eu estou pegando todos os elemento que já estão na estatisticas
@@ -79,3 +79,4 @@ function atualizaEstatisticas(peca) {
         elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
     })
 }
+
